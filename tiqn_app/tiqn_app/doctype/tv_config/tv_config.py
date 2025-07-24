8766992134b7b7cb42aba6c5e8030d4cc3af2942
nguyenson-toray <son.nt@tiqn.com.vn>
2025-07-24 12:45:34 +0700

    add TV config

---
+++ tiqn_app/tiqn_app/doctype/tv_config/tv_config.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, IT Team and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TVConfig(Document):
+	pass
