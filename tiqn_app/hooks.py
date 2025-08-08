ff2bea821417a32c0825958f61a7408d192155b5
nguyenson-toray <sont.nt@tiqn.com.vn>
2025-08-08 22:16:11 +0700

    remove hook of tiqn_app

--- tiqn_app/hooks.py
+++ tiqn_app/hooks.py
@@ -17,45 +17,45 @@ app_license = "mit"
     # 2 : Chạy lệnh bench --site erp.tiqn.local clear-cache
     # 3 : Chạy lệnh bench --site erpt.tiqn.local migrate    
     # hoặc cho 1 app cụ thể : bench --site erp.tiqn.local import-fixtures --app customize_erpnext  
-fixtures = [ 
-     {
-        "doctype": "Custom Field",
-        "filters": [
-            [
-                "name",
-                "in",
-                [
-                    # "Stock Entry-custom_%",
-                    # "Sales Order Item-custom_%", 
-                    # "Sales Order-section_break_%",
-                    # "Sales Order-sum_by_country", 
-                    # "Sales Order-sum_by_delivery_date", 
-                    # "Sales Order-sum_by_sku",
-                    # "BOM Item-custom_%", 
-                    # "Material Request Item-custom_%",
-                    # "Production Plan-custom_%",
-                    # "Employee-custom_%", 
-                ]
-            ]
-        ]
-    },
+# fixtures = [ 
+    #  {
+    #     "doctype": "Custom Field",
+    #     "filters": [
+    #         [
+    #             "name",
+    #             "in",
+    #             [
+    #                 # "Stock Entry-custom_%",
+    #                 # "Sales Order Item-custom_%", 
+    #                 # "Sales Order-section_break_%",
+    #                 # "Sales Order-sum_by_country", 
+    #                 # "Sales Order-sum_by_delivery_date", 
+    #                 # "Sales Order-sum_by_sku",
+    #                 # "BOM Item-custom_%", 
+    #                 # "Material Request Item-custom_%",
+    #                 # "Production Plan-custom_%",
+    #                 # "Employee-custom_%", 
+    #             ]
+    #         ]
+    #     ]
+    # },
     # Custom Workspace
-    {
-        "doctype": "Workspace",
-        "filters": [
-            ["name", "in", ["Registration"]]
-        ]
-    },
+    # {
+    #     "doctype": "Workspace",
+    #     "filters": [
+    #         ["name", "in", ["Registration"]]
+    #     ]
+    # },
     # Property Setter
-    {
-        "doctype": "Property Setter",
-        "filters": [
-            ["doc_type", "in", [
-                # "Sales Invoice", "Purchase Invoice"
-                ]]
-        ]
-    }
-]
+    # {
+    #     "doctype": "Property Setter",
+    #     "filters": [
+    #         ["doc_type", "in", [
+    #             # "Sales Invoice", "Purchase Invoice"
+    #             ]]
+    #     ]
+    # }
+# ]
 
 
 # required_apps = []
